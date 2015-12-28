c1413e505126a9728d0760c833e42a3d7c3c6f01
Fraideron <jiafen@yandex.ua>
2015-12-29 00:28:21 +0200

    Update Calculator.cpp

--- qualityWorkWithTasks/calculator/Calculator.cpp
+++ qualityWorkWithTasks/calculator/Calculator.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+#include "console.h"
+#include "simpio.h"  // for getLine
+#include "vector.h"  // for Vector
 #include "stack.h"
-#include <vector>
-#include <cstdlib>
-#include <sstream>
 #include <math.h>
 
 using namespace std;
 
 /**
- * @brief The Calculator class  using an 
+ * @brief The Calculator class  using an
  *        algorithm sorting station has introduced expression
  */
 class Calculator
@@ -18,19 +18,19 @@ private:
     Stack<string> * tempStack ;
     vector<string> * polishNotation;
 public:
-    Calculator(string function = "2+2") {
+    Calculator(string function) {
         inputString = function;
         tempStack = new Stack<string>;
         polishNotation = new vector<string>;
     }
-
+    
     ~Calculator() {
         delete tempStack;
         delete polishNotation;
     }
-
-
-
+    
+    
+    
     /**
      * @brief getTokenType - function returns the value of int to separate tokens
      * @param token
@@ -46,8 +46,9 @@ public:
         } else if(token == '(' || token == ')'){
             return 3;
         }
+        return -1;
     }
-
+    
     /**
      * @brief getOneTokenFromInputString - function returns a token starting with position 'i'
      * @param position - position of token start
@@ -56,22 +57,22 @@ public:
      */
     string getOneTokenFromInputString(int& position, string & functionString){
         string outputString;
-
+        
         char current = functionString[position];
         char next = functionString[position+1];
-
+        
         while ((getTokenType(current) == getTokenType(next)) && position < functionString.size()) {
             outputString += current;
             position++;
             current = next;
             next = functionString[position+1];
         }
-
+        
         outputString += current;
         ++position;
         return outputString;
     }
-
+    
     /**
      * @brief getTokenPriority - priority function returns each input token
      * @param token - token to check
@@ -90,19 +91,19 @@ public:
             return 3;
         }
     }
-
-
-
+    
+    
+    
     /**
-     * @brief getPolishNotationFromString - using a standard algorithm, 
-     *                                      we obtain the inverse Polish notation 
+     * @brief getPolishNotationFromString - using a standard algorithm,
+     *                                      we obtain the inverse Polish notation
      *                                      from the input line
      */
-    getPolishNotationFromString(){
+    void getPolishNotationFromString(){
         for (int i = 0; i < inputString.size(); ) {
             string token = getOneTokenFromInputString(i, inputString);
             int tokenType = getTokenPriority(token[0]);
-
+            
             if(isdigit(token[0]))  {
                 polishNotation->push_back(token);
             } else if (token == "(") {
@@ -118,7 +119,7 @@ public:
                     tempStack->push(token);
                     continue;
                 }
-
+                
                 while((!tempStack->empty()) && tokenType < getTokenPriority(tempStack->top()[0]) ){
                     polishNotation->push_back(tempStack->top());
                     tempStack->pop();
@@ -126,17 +127,17 @@ public:
                 tempStack->push(token);
             }
         }
-
+        
         while (!tempStack->empty()) {
             polishNotation->push_back(tempStack->top());
             tempStack->pop();
         }
-
+        
     }
     /**
      * @brief stringToDouble convert  string to double
      * @param str - input data
-     * @return 
+     * @return
      */
     double stringToDouble(string str){
         stringstream ss(str);
@@ -144,7 +145,7 @@ public:
         ss >> convertedValue;
         return convertedValue;
     }
-
+    
     /**
      * @brief doubleToString convert double to string
      * @param doub
@@ -155,10 +156,10 @@ public:
         strs << doub;
         string str = strs.str();
         return str;
-
+        
     }
     /**
-     * @brief getFunctionParamNumber - function returns the number of operands 
+     * @brief getFunctionParamNumber - function returns the number of operands
      *                                 required for the operation
      * @param token - operation
      * @return int value
@@ -176,21 +177,21 @@ public:
             return 0;
         }
     }
-
+    
     /**
-     * @brief calculateInputFunction - using inverse Polish notation, 
-     *                                 this function is operation calculates 
-     *                                 the required number of operands, 
-     *                                 perform actions on the data obtained, 
+     * @brief calculateInputFunction - using inverse Polish notation,
+     *                                 this function is operation calculates
+     *                                 the required number of operands,
+     *                                 perform actions on the data obtained,
      *                                 deletes them and adds the result in the removal of seat
      */
     void calculateInputFunction(){
-
-
+        
+        
         for (int i = 0; i < polishNotation->size(); i++) {
             string token = polishNotation->at(i);
-
-
+            
+            
             if (getFunctionParamNumber(token) == 2) {
                 int firstArg = stringToDouble(polishNotation->at(i-1));
                 int secondArg = stringToDouble(polishNotation->at(i-2));
@@ -206,11 +207,12 @@ public:
                 } else if (token == "^"){
                     rezult = pow(firstArg, secondArg);
                 }
-
+                
                 polishNotation->erase(polishNotation->begin()+i);
                 polishNotation->erase(polishNotation->begin()+i-2);
                 polishNotation->erase(polishNotation->begin()+i-2);
                 polishNotation->insert(polishNotation->begin()+i-2, doubleToString(rezult));
+                i -= 2;
             } else if(getFunctionParamNumber(token) == 1){
                 int firstArg = stringToDouble(polishNotation->at(i-1));
                 double rezult;
@@ -224,28 +226,34 @@ public:
                 polishNotation->insert(polishNotation->begin()+i-1, doubleToString(rezult));
                 i--;
             }
-
+            
         }
         cout << (*polishNotation)[0];
-
+        
     }
 };
 
-
-
-
-
-int main()
-{
-    
-
-
-    Calculator calc;
-    calc.getPolishNotationFromString();
-
-
-    calc.calculateInputFunction();
-
-    return 0;
+void performCalculations(string formula){
+    Calculator *calc = new Calculator(formula);
+    calc->getPolishNotationFromString();
+    calc->calculateInputFunction();
+    delete calc;
 }
 
+
+int main() {
+    
+    string formula = getLine("You can use next tokens: cos(token), sin(token), token^ token, and other standart operations. \nEnter the data for calculating, please: ");
+    cout << formula <<  " = ";
+    performCalculations(formula);
+    string check = getLine("\nOne's more? (y/n)");
+    while(check == "y"){
+        formula = getLine("\nEnter the data for calculating, please: ");
+        cout << endl << formula <<  " = ";
+        performCalculations(formula);
+        string check = getLine("\nOne's more? (y/n)");
+    }
+    
+    
+    return 0;
+}
